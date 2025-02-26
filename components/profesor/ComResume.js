2f17e43d6caf1aba03927a22b3ab5439db6bffc5
Raimundo Escalona <rjescalona@uc.cl>
2025-02-26 16:20:28 -0300

    refactor: improve styling and layout of the teacher's resume component

--- components/profesor/ComResume.js
+++ components/profesor/ComResume.js
@@ -35,21 +35,21 @@ const Resume = ({ average, reviewAmount, oneStarReviews, twoStarReviews, threeSt
 
   return (
     <div className="md:w-1/2 lg:w-1/3 m-2 py-3 px-5 bg-white rounded-md shadow-md">
-        <div className='flex place-items-center px-3'>
-            <Star filled={true} color={"yellow-500"} size={5} />
-            <h1 className='font-bold ml-1'>{roundToTwo(average)} <span className='font-normal text-gray-500'>({reviewAmount} reseñas)</span></h1>
+        <div className='flex items-center px-3'>
+            <Star filled={true} color={"yellow-500"} size={4} />
+            <h1 className='font-bold ml-2'>{roundToTwo(average)} <span className='font-normal text-gray-500'>({reviewAmount} reseñas)</span></h1>
         </div>
         <div className='p-0'>
             {[5, 4, 3, 2, 1].map(starRating => (
                 <div key={starRating} className="bg-gray-200 rounded-md h-6 my-2 p-0 overflow-hidden relative group">
-                    <div style={{ width: `${starPercentages[5 - starRating]?.percentage}%` }} className={`h-6 bg-yellow-400 place-items-center px-0 align-middle`}>
-                        <div className='flex w-screen place-items-center h-full mx-2 items-center align-middle'>
-                            <div className='flex items-center align-middle h-fit'>
+                    <div style={{ width: `${starPercentages[5 - starRating]?.percentage}%` }} className={`h-6 bg-yellow-400 flex items-center px-0`}>
+                        <div className='flex items-center h-full mx-2'>
+                            <div className='flex items-center'>
                                 {[...Array(starRating)].map((_, index) => (
                                     <Star key={index} filled={true} color={"black"} size={3} />
                                 ))}
                             </div>
-                            <h1 className='font-semibold ml-1 text-sm'>{starsCount[5 - starRating]} <span className='font-normal'>({starPercentages[5 - starRating]?.percentage}%)</span></h1>
+                            <h1 className='font-semibold ml-2 text-sm whitespace-nowrap'>{starsCount[5 - starRating]} <span className='font-normal'>({starPercentages[5 - starRating]?.percentage}%)</span></h1>
                         </div>
                     </div>
                     <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
